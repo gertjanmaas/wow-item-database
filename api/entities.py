7a41711dc7a13608ef8f39e11914759c4ce93b92
Gertjan Maas <gertjan@maas.codes>
2019-11-12 15:24:05 +0100

    Add wowhead tooltips

--- api/entities.py
+++ api/entities.py
@@ -15,17 +15,20 @@ class Item(db.Model):
     name = db.Column(db.String(100))
     quality = db.Column(db.Enum(ItemQuality))
     icon = db.Column(db.String(50))
+    wowhead_id = db.Column(db.Integer)
 
-    def __init__(self,id=None, name=None, quality=None, icon=None):
+    def __init__(self,id=None, name=None, quality=None, icon=None, wowhead_id=None):
         self.id = id
         self.name = name
         self.quality = quality
         self.icon = icon
+        self.wowhead_id = wowhead_id
 
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name,
             'quality': self.quality,
+            'wowhead_id': self.wowhead_id,
             'icon': f"/static/icons/{self.icon}.jpg"
         }
--- api/service.py
+++ api/service.py
@@ -25,17 +25,17 @@ def list_items():
 
 def populate(db):
     items = [
-        Item(name="Thunderfury, Blessed Blade of the Windseeker", quality=ItemQuality.LEGENDARY, icon="inv_sword_39"),
-        Item(name="Orb of Deception", quality=ItemQuality.RARE, icon="inv_misc_orb_02"),
-        Item(name="The Unstoppable Force", quality=ItemQuality.EPIC, icon="inv_hammer_13"),
-        Item(name="Noggenfogger Elixir", quality=ItemQuality.COMMON, icon="inv_potion_83"),
-        Item(name="Righteous Orb", quality=ItemQuality.UNCOMMON, icon="inv_misc_gem_pearl_03"),
-        Item(name="Nat Pagle's Guide to Extreme Anglin'", quality=ItemQuality.JUNK, icon="inv_misc_book_02"),
-        Item(name="Savory Deviate Delight", quality=ItemQuality.COMMON, icon="inv_misc_monsterhead_04"),
-        Item(name="Carrot on a Stick", quality=ItemQuality.UNCOMMON, icon="inv_misc_food_54"),
-        Item(name="Skullflame Shield", quality=ItemQuality.EPIC, icon="inv_shield_01"),
-        Item(name="Sulfuras, Hand of Ragnaros", quality=ItemQuality.LEGENDARY, icon="inv_hammer_unique_sulfuras"),
-        Item(name="Rusty Warhammer", quality=ItemQuality.JUNK, icon="inv_hammer_16")
+        Item(name="Thunderfury, Blessed Blade of the Windseeker", quality=ItemQuality.LEGENDARY, icon="inv_sword_39", wowhead_id=19019),
+        Item(name="Orb of Deception", quality=ItemQuality.RARE, icon="inv_misc_orb_02", wowhead_id=1973),
+        Item(name="The Unstoppable Force", quality=ItemQuality.EPIC, icon="inv_hammer_13", wowhead_id=19323),
+        Item(name="Noggenfogger Elixir", quality=ItemQuality.COMMON, icon="inv_potion_83", wowhead_id=8529),
+        Item(name="Righteous Orb", quality=ItemQuality.UNCOMMON, icon="inv_misc_gem_pearl_03", wowhead_id=12811),
+        Item(name="Nat Pagle's Guide to Extreme Anglin'", quality=ItemQuality.JUNK, icon="inv_misc_book_02", wowhead_id=18229),
+        Item(name="Savory Deviate Delight", quality=ItemQuality.COMMON, icon="inv_misc_monsterhead_04", wowhead_id=6657),
+        Item(name="Carrot on a Stick", quality=ItemQuality.UNCOMMON, icon="inv_misc_food_54", wowhead_id=11122),
+        Item(name="Skullflame Shield", quality=ItemQuality.EPIC, icon="inv_shield_01", wowhead_id=1168),
+        Item(name="Sulfuras, Hand of Ragnaros", quality=ItemQuality.LEGENDARY, icon="inv_hammer_unique_sulfuras", wowhead_id=17182),
+        Item(name="Rusty Warhammer", quality=ItemQuality.JUNK, icon="inv_hammer_16", wowhead_id=1514)
     ]
 
     for i in items:
